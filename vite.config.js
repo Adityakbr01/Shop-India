024a37c724b5b5f48df07ede1d9e7282f9328c8b
Adityakbr01 <aditykbr01@gmail.com>
2024-07-23 01:01:10 +0530

    2 commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/your-subdirectory/', // If deploying to a subdirectory
-})
+  base: "./dist", // If deploying to a subdirectory
+});
